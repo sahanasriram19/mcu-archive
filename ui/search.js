da028807da699dafc53b842442de7ae990c0d65b
sahanasriram19 <sahana19.25@ichat.sp.edu.sg>
2026-07-28 09:59:12 +0800

    made search feature zoom better

--- ui/search.js
+++ ui/search.js
@@ -183,7 +183,7 @@ export function initMovieSearch(){
 
         camera.targetX=node.x;
         camera.targetY=node.y;
-        camera.targetZoom=0.55;
+        camera.targetZoom=0.75;
 
         results=[];
 
